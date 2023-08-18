0f3b2deb38bf3e1ec36d2f1c8389e61299b7aba5
arshan_alee <mirarshanali@gmail.com>
2023-08-18 12:39:56 +0500

    Portfolio updates

---
+++ src/Components/PortfolioContainer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function PortfolioContainer(props) {
+  return (
+    <div className="portfolio--section--container">
+      {props.portfolioData?.map((item, index) => (
+        <div key={index} className="portfolio--section--card">
+          <div className="portfolio--section--img">
+            <img src={item.src} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">{item.title}</h3>
+              <p className="text-md">{item.description}</p>
+            </div>
+            <a
+              href={item.link}
+              className="text-sm portfolio--link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View In GitHub
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,12 +1,12 @@
 import data from "../../data/index.json";
-
+import PortfolioContainer from "../../Components/PortfolioContainer";
 export default function MyPortfolio() {
   return (
     <section className="portfolio--section" id="MyPortfolio">
       <div className="portfolio--container-box">
         <div className="portfolio--container">
           <p className="sub--title">Recent Projects</p>
-          <h2 className="section--heading">My Portfolio</h2>
+          <h2 className="skills--section--heading">My Portfolio</h2>
         </div>
         <div>
           <a href="https://github.com/your-username" target="_blank" rel="noreferrer" className="btn btn-github">
@@ -28,39 +28,10 @@ export default function MyPortfolio() {
           </a>
         </div>
       </div>
-      <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
-            </div>
-            <div className="portfolio--section--card--content">
-              <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
-              </div>
-              <p className="text-sm portfolio--link">
-                {item.link}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <h2 className="section--heading">Web Applications</h2>
+      <PortfolioContainer portfolioData={data?.portfolio} />
+      <h2 className="section--heading">Mobile Applications</h2>
+      <PortfolioContainer portfolioData={data?.portfolio} />
     </section>
   );
 }
